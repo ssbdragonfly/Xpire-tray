d85baf6954dae83358232fa3c9b83a44431c1b77
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-02-10 16:03:13 -0500

    Rename gui_utils file dialog

--- gui_utils.py
+++ gui_utils.py
@@ -1,9 +1,9 @@
 import customtkinter as ctk
 from pathlib import Path
 
-def file_data() -> Path:
+def select_file() -> Path:
     fpath = Path(ctk.filedialog.askopenfilename())
     if not fpath.exists():
         raise RuntimeError("CTk Failed somewhere")
-    return fpath
+    return fpath.absolute()
 
