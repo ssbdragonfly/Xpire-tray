ed2ef88c7f93a13043fd0b94050e47ec4d6ef65d
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-02-10 09:47:46 -0500

    Add main GUI file

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import customtkinter as ctk
+
+
+
+class GUI(ctk.CTk):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+def main():
+    GUI().mainloop()
+
+if __name__ == "__main__":
+    main()
