87a59286c3eb08243fd981cdc68f757a98c5051c
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-02-10 11:06:07 -0500

    Update backend (almost finished parsing)

--- backend/data.py
+++ backend/data.py
@@ -4,7 +4,8 @@ from dataclasses import dataclass
 @dataclass
 class FoodData:
     name: str
-    pantry: int
-    fridge: int
-    freezer: int
+    pantry: int = -1
+    fridge: int = -1
+    fridge_after_opening: int = -1
+    freezer: int = -1
 
--- backend/parse_foods_expiry.py
+++ backend/parse_foods_expiry.py
@@ -5,28 +5,43 @@ import csv
 
 def get_food_info():
     """Returns a dictionary mapping a food name to a `FoodData`
-    instance. This dataclass contains the pantry, fridge, and freezer attributes.
+    instance. This dataclass contains the pantry, fridge, fridge_after_open, and freezer attributes.
     If the item should not be in a {pantry, fridge, freezer} the attribute will be -1
     """
+    foods = {}
     with FOOD_DATA_PATH.open(newline="", encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-    foods = {}
-    for row in reader:
-        name = row[2]
-        data = FoodData(
-            name = name,
-            pantry = _get_timelength(row, 6),
-            fridge = _get_timelength(row, 16)
-        )
+        for row in reader:
+            if row[0] == "ID":
+                continue # skip headers
+            name = row[2]
+            print("hi!", row[21], row)
+            data = FoodData(
+                name = name,
+                pantry = _get_timelength(row, 6, 3),
+                fridge = _get_timelength(row, 17, 3),
+                fridge_after_opening = _get_timelength(row, 25, buffer=0),
+                freezer = _get_timelength(row, 31)
+            )
+            foods[name] = data
+            print(data)
 
-def _get_timelength(row: list[str], index: int) -> int:
-    return max(_parse_timelength(row[index], row[index+1]), _parse_timelength(row[index+3], row[index+4]))
+def _get_timelength(row: list[str], index: int, buffer: int = 3) -> int:
+    return max(
+        _parse_timelength(row[index], row[index+1]),
+        _parse_timelength(row[index+buffer+1], row[index+buffer+2])
+    )
 
 def _parse_timelength(length: str, unit: str) -> int:
     """Converts a length to days"""
     factors = {
         "Days": 1,
         "Weeks": 7,
+        "Months": 30,
+        "Years": 365
     }
-    return int(length)*factors[unit] if length else -1
+    try:
+        return int(length)*factors[unit] if length else -1
+    except (KeyError, ValueError):
+        return -1 # empty values
 
