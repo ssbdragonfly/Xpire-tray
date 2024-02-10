9145510e3d374fd9c3af15e10666493b5b5cf168
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-02-10 16:38:41 -0500

    Only allow certain filetypes for file dialog

--- gui_utils.py
+++ gui_utils.py
@@ -2,7 +2,19 @@ import customtkinter as ctk
 from pathlib import Path
 
 def select_file() -> Path:
-    fpath = Path(ctk.filedialog.askopenfilename())
+    ftypes = ("png", "jpeg", "svg")
+    fpath = Path(
+        ctk.filedialog.askopenfilename(
+            title="Select a File",
+            filetypes=[
+                *[
+                    ("{x.upper()} files", f"*.{x}")
+                    for x in ftypes
+                ],
+                ("All files", "*.*")
+            ]
+        )
+    )
     if not fpath.exists():
         raise RuntimeError("CTk Failed somewhere")
     return fpath.absolute()
