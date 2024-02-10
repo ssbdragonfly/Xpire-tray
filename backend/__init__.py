1b971fad9746d74dcf1275d87a0f9b81534277ac
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-02-10 13:35:38 -0500

    Update backend to cache foods dict

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,2 +1,2 @@
-from backend.parse_foods_expiry import get_food_info
 from backend.dates import schedule_reminders
+from backend.utils import *
--- backend/utils.py
+++ backend/utils.py
@@ -4,6 +4,12 @@ import pickle
 from pathlib import Path
 from backend.parse_foods_expiry import get_food_info
 
+__all__ = [
+    "kill_notifs",
+    "search_for",
+    "get_foods_cached"
+]
+
 def kill_notifs() -> None:
     for proc in psutil.process_iter():
         if proc.name() == "main.py":
@@ -17,10 +23,10 @@ def search_for(item: str, foods: dict) -> str | list[str]:
 def get_foods_cached():
     path = Path("storage/foods.pickle")
     if path.exists():
-        with path.open("r") as f:
+        with path.open("rb") as f:
             return pickle.load(f)
     foods = get_food_info()
-    with path.open("w") as f:
+    with path.open("wb") as f:
         pickle.dump(foods, f)
     return foods
 
