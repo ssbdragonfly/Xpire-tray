f7d2fe5685e2ad2420faad77e4f36319225260ab
ParthivM1 <parthiv.maddipatla@gmail.com>
2024-02-11 09:28:18 -0500

    Changed ordering and added functionality

--- backend/assess.py
+++ backend/assess.py
@@ -1,14 +1,12 @@
-import tkinter as tk
+import customtkinter as ctk
+ctk.set_appearance_mode('dark')
 def assessing(carbs_percent, sugar_percent, sodium_percent, protein_percent, fat_percent):
-    window = tk.Tk()
+    window = ctk.CTk()
     window.title("Nutrition Assessment")
-    window.configure(bg='black')  
     window.geometry("700x350")    
-    default_font = ('Arial', 14)
-    default_fg = 'black'
-    nutrition_text = tk.Text(window, height=10, width=80, bg='grey', fg='white', font=default_font)
+    nutrition_text = ctk.CTkTextbox(window, height=200, width=500)
     nutrition_text.pack(pady=10)
-    button_exit = tk.Button(window, text="Close", command=window.destroy, fg='black', bg='blue', font=default_font)
+    button_exit = ctk.CTkButton(window, text="Close", command=window.destroy)
     button_exit.pack(pady=10)
     healthy_ranges = {
         'carbs': {'range': (45, 65), 'weight': 0.2},
@@ -32,11 +30,11 @@ def assessing(carbs_percent, sugar_percent, sodium_percent, protein_percent, fat
             nutrient_assessment[nutrient] = 'is too high, consider lower consumption of it.'
             health_index -= (nutrient_percent - upper_bound) * weight * 10
     
-    nutrition_text.insert(tk.END,'Health Index is:'+str(health_index)+'\n')
-    nutrition_text.insert(tk.END, 'Carbohydrate Consumption '+nutrient_assessment['carbs']+'\n')
-    nutrition_text.insert(tk.END, 'Sugar Consumption '+nutrient_assessment['sugar']+'\n')
-    nutrition_text.insert(tk.END, 'Sodium Consumption '+nutrient_assessment['sodium']+'\n')
-    nutrition_text.insert(tk.END, 'Protein Consumption '+nutrient_assessment['protein']+'\n')
-    nutrition_text.insert(tk.END, 'Fat Consumption '+nutrient_assessment['fat'])
+    nutrition_text.insert("end",'Health Index is:'+str(health_index)+'\n')
+    nutrition_text.insert("end", 'Carbohydrate Consumption '+nutrient_assessment['carbs']+'\n')
+    nutrition_text.insert("end", 'Sugar Consumption '+nutrient_assessment['sugar']+'\n')
+    nutrition_text.insert("end", 'Sodium Consumption '+nutrient_assessment['sodium']+'\n')
+    nutrition_text.insert("end", 'Protein Consumption '+nutrient_assessment['protein']+'\n')
+    nutrition_text.insert("end", 'Fat Consumption '+nutrient_assessment['fat'])
     window.mainloop()
     return lambda:None
