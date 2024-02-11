d5b74ccc0217966de07b80b47f0df1434a4fc5dc
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-02-10 20:51:51 -0500

    Cleaned up main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import customtkinter as ctk
 from backend import handle_input
 
-ctk.set_appearance_mode("Dark")   
+ctk.set_appearance_mode("dark")   
 ctk.set_default_color_theme("blue")
 
 width, height = 1000, 600
@@ -11,20 +11,33 @@ class App(ctk.CTk):
         self.title("Food Expire-E Program")   #change the name later
         self.geometry(f"{width}x{height}")    
 
+        pack_kwargs = {
+            "pady": 20,
+            "padx": 20,
+            "anchor": "w"
+        }
+
         # Products Label
-        self.product_label = ctk.CTkLabel(self, text="Products")
-        self.product_label.pack(pady=20, padx=20, anchor="w")
+        self.product_label = ctk.CTkLabel(
+            self,
+            text="Products"
+        )
+        self.product_label.pack(**pack_kwargs)
  
         # Product Entry Field
-        self.product_entry = ctk.CTkEntry(self,
-                            placeholder_text="Comma seperated list!")
-        self.product_entry.pack(pady=20, padx=20, anchor="w")
+        self.product_entry = ctk.CTkEntry(
+            self,
+            placeholder_text="Comma seperated list!"
+        )
+        self.product_entry.pack(**pack_kwargs)
 
         # Generate Button
-        self.gen_results_button = ctk.CTkButton(self,
-                                         text="Generate Results",
-                                         command=self.create_text)
-        self.gen_results_button.pack(pady=20, padx=20, anchor="w")
+        self.gen_results_button = ctk.CTkButton(
+            self,
+            text="Generate Results",
+            command=self.create_text
+        )
+        self.gen_results_button.pack(**pack_kwargs)
 
     @staticmethod
     def filter_input(s: str) -> list[str]:
@@ -38,6 +51,9 @@ class App(ctk.CTk):
         expireitems = [x for x in self.filter_input(expireitems) if x]
         handle_input(expireitems)
 
-if __name__ == "__main__":
+def main():
     app = App()
     app.mainloop()
+
+if __name__ == "__main__":
+    main()
