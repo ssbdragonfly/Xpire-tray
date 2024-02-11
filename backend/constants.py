e5c45977dce312f8d4dda70cb8b0ee5c44a606c7
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-02-10 20:44:21 -0500

    Remove data folder

--- backend/constants.py
+++ backend/constants.py
@@ -1,8 +1,3 @@
-IMAGEURL = ""
-IMAGE_API_TOKEN = ""
-IMAGE_HEADERS = {"Authorization": "Bearer " + IMAGE_API_TOKEN}
-
-
 from pathlib import Path
-FOOD_DATA_PATH = Path("data/foods.csv")
+FOOD_DATA_PATH = Path("storage/foods.csv")
 
